f050ee6d271b6859bbde7902df020965875f4051
hongquant17 <hongquant.1712@gmail.com>
2023-10-22 21:55:28 +0700

    feat: set up project

---
+++ app/src/main/java/com/HarmonyHub/musicPlayer/AboutActivity.kt
@@ -0,0 +1,22 @@
+package com.HarmonyHub.musicPlayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.HarmonyHub.musicPlayer.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(MainActivity.currentThemeNav[MainActivity.themeIndex])
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "About"
+        binding.aboutText.text = aboutText()
+    }
+    private fun aboutText(): String{
+        return "Project Harmony Hub"
+    }
+}
