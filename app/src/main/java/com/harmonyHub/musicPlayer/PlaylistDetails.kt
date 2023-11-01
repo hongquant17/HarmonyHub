1a06172d5ddfba0e39e61ea1d3543a7c083a8bfb
Tan Duc <ductan10092003@gmail.com>
2023-11-02 02:30:55 +0700

    playlist view demo

--- app/src/main/java/com/harmonyHub/musicPlayer/PlaylistDetails.kt
+++ app/src/main/java/com/harmonyHub/musicPlayer/PlaylistDetails.kt
@@ -36,12 +36,12 @@ class PlaylistDetails : AppCompatActivity() {
         adapter = MusicAdapter(this, PlaylistActivity.musicPlaylist.ref[currentPlaylistPos].playlist, playlistDetails = true)
         binding.playlistDetailsRV.adapter = adapter
         binding.backBtnPD.setOnClickListener { finish() }
-        binding.shuffleBtnPD.setOnClickListener {
-            val intent = Intent(this, PlayerActivity::class.java)
-            intent.putExtra("index", 0)
-            intent.putExtra("class", "PlaylistDetailsShuffle")
-            startActivity(intent)
-        }
+//        binding.shuffleBtnPD.setOnClickListener {
+//            val intent = Intent(this, PlayerActivity::class.java)
+//            intent.putExtra("index", 0)
+//            intent.putExtra("class", "PlaylistDetailsShuffle")
+//            startActivity(intent)
+//        }
         binding.addBtnPD.setOnClickListener {
             startActivity(Intent(this, SelectionActivity::class.java))
         }
@@ -68,17 +68,18 @@ class PlaylistDetails : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         binding.playlistNamePD.text = PlaylistActivity.musicPlaylist.ref[currentPlaylistPos].name
-        binding.moreInfoPD.text = "Total ${adapter.itemCount} Songs.\n\n" +
-                "Created On:\n${PlaylistActivity.musicPlaylist.ref[currentPlaylistPos].createdOn}\n\n" +
-                "  -- ${PlaylistActivity.musicPlaylist.ref[currentPlaylistPos].createdBy}"
-        if(adapter.itemCount > 0)
-        {
-            Glide.with(this)
-                .load(PlaylistActivity.musicPlaylist.ref[currentPlaylistPos].playlist[0].artUri)
-                .apply(RequestOptions().placeholder(R.drawable.music_player_icon_slash_screen).centerCrop())
-                .into(binding.playlistImgPD)
-            binding.shuffleBtnPD.visibility = View.VISIBLE
-        }
+//        binding.moreInfoPD.text = "Total ${adapter.itemCount} Songs.\n\n" +
+//                "Created On:\n${PlaylistActivity.musicPlaylist.ref[currentPlaylistPos].createdOn}\n\n" +
+//                "  -- ${PlaylistActivity.musicPlaylist.ref[currentPlaylistPos].createdBy}"
+        binding.moreInfoPD.text = "${adapter.itemCount} songs to sing in the shower."
+//        if(adapter.itemCount > 0)
+//        {
+//            Glide.with(this)
+//                .load(PlaylistActivity.musicPlaylist.ref[currentPlaylistPos].playlist[0].artUri)
+//                .apply(RequestOptions().placeholder(R.drawable.music_player_icon_slash_screen).centerCrop())
+//                .into(binding.playlistImgPD)
+//            binding.shuffleBtnPD.visibility = View.VISIBLE
+//        }
         adapter.notifyDataSetChanged()
         //for storing favourites data using shared preferences
         val editor = getSharedPreferences("FAVOURITES", MODE_PRIVATE).edit()
