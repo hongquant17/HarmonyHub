3cf60f2205a6d93e3783850cbb76234f1cc7e299
Tan Duc <ductan10092003@gmail.com>
2023-12-17 00:53:30 +0700

    test fe

--- app/src/main/java/com/harmonyHub/musicPlayer/OnlinePlaying.kt
+++ app/src/main/java/com/harmonyHub/musicPlayer/OnlinePlaying.kt
@@ -12,7 +12,7 @@ class OnlinePlaying : AppCompatActivity() {
 
     private lateinit var thumbnail: MutableList<String>
     private lateinit var jcPlayerView1: JcPlayerView
-    private lateinit var jcPlayerView2: JcPlayerView
+//    private lateinit var jcPlayerView2: JcPlayerView
     private lateinit var jcAudios: List<JcAudio>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +21,7 @@ class OnlinePlaying : AppCompatActivity() {
         supportActionBar?.hide() // For Hiding The Action bar
 
         jcPlayerView1 = findViewById(R.id.jcplayer1)
-        jcPlayerView2 = findViewById(R.id.jcplayer2)
+//        jcPlayerView2 = findViewById(R.id.jcplayer2)
         val songId = intent.getIntExtra("songId", -1)
         thumbnail = intent.getStringArrayListExtra("thumbnail") as MutableList<String>
         jcAudios = intent.getParcelableArrayListExtra<JcAudio>("jcAudios") ?: emptyList()
@@ -35,14 +35,14 @@ class OnlinePlaying : AppCompatActivity() {
 
     private fun initializeJcPlayerViews(jcAudios: List<JcAudio>, id: Int, thumbnail: List<String>) {
         jcPlayerView1.initPlaylist(jcAudios)
-        jcPlayerView2.initPlaylist(jcAudios)
+//        jcPlayerView2.initPlaylist(jcAudios)
 
         if (id >= 0 && id < jcAudios.size) {
             jcPlayerView1.playAudio(jcAudios[id])
             jcPlayerView1.createNotification(R.drawable.notimg)
 
-            jcPlayerView2.playAudio(jcAudios[id])
-            jcPlayerView2.createNotification(R.drawable.notimg)
+//            jcPlayerView2.playAudio(jcAudios[id])
+//            jcPlayerView2.createNotification(R.drawable.notimg)
         }
 
         val imageView2: ImageView = findViewById(R.id.imageView2)
