4111e0a9814f7a6d683076227a7340ad74234525
NguyenOanhy <21020373@vnu.edu.vn>
2023-12-18 23:27:23 +0700

    album top, hot songs

--- app/src/main/java/com/harmonyHub/musicPlayer/OnlineMusic.kt
+++ app/src/main/java/com/harmonyHub/musicPlayer/OnlineMusic.kt
@@ -2,7 +2,6 @@ package com.harmonyHub.musicPlayer
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
@@ -14,42 +13,37 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class OnlineMusic : AppCompatActivity() {
 
-    private lateinit var listView: ListView
-    private lateinit var albumView: ListView
+    private lateinit var topListView: ListView
+    private lateinit var bottomListView: ListView
     private lateinit var songsNameList: MutableList<String?>
     private lateinit var songsUrlList: MutableList<String?>
     private lateinit var songsArtistList: MutableList<String?>
     private lateinit var songsDurationList: MutableList<String?>
-    private lateinit var adapter: ListAdapter
     private lateinit var jcPlayerView: JcPlayerView
     private lateinit var jcAudios: MutableList<JcAudio>
     private lateinit var thumbnail: MutableList<String?>
     private lateinit var albumList: MutableList<Album>
     private lateinit var albumAdapter: AlbumAdapter
+    private lateinit var albumAdapterTop: AlbumAdapter
+    private lateinit var albumAdapterBottom: AlbumAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_online_music)
 
         initializeViews()
-        retrieveSongs()
         retrieveAlbums()
 
-        listView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
-            jcPlayerView.visibility = View.VISIBLE
-            jcPlayerView.createNotification()
-            adapter.notifyDataSetChanged()
-            val selectedSong = position
-            val intent = Intent(this, OnlinePlaying::class.java)
-            intent.putExtra("songId", selectedSong)
-            intent.putExtra("thumbnail", ArrayList(thumbnail))
-            intent.putParcelableArrayListExtra("jcAudios", ArrayList(jcAudios))
+        topListView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
+            albumAdapter.notifyDataSetChanged()
+            val selectedAlbum = (position + 1).toString()
+            val intent = Intent(this, OnlineAlbum::class.java)
+            intent.putExtra("albumId", selectedAlbum)
             startActivity(intent)
         }
-
-        albumView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
+        bottomListView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
             albumAdapter.notifyDataSetChanged()
-            val selectedAlbum = (position +1).toString()
+            val selectedAlbum = (position + 4).toString()
             val intent = Intent(this, OnlineAlbum::class.java)
             intent.putExtra("albumId", selectedAlbum)
             startActivity(intent)
@@ -57,8 +51,8 @@ class OnlineMusic : AppCompatActivity() {
     }
 
     private fun initializeViews() {
-        listView = findViewById(R.id.songsList)
-        albumView = findViewById(R.id.albumList)
+        topListView = findViewById(R.id.topListView)
+        bottomListView = findViewById(R.id.bottomListView)
         songsNameList = mutableListOf()
         songsUrlList = mutableListOf()
         songsArtistList = mutableListOf()
@@ -68,47 +62,7 @@ class OnlineMusic : AppCompatActivity() {
         jcPlayerView = findViewById(R.id.jcplayer)
         albumList = mutableListOf()
         albumAdapter = AlbumAdapter(this, albumList)
-        val albumListView = findViewById<ListView>(R.id.albumList)
-        albumListView.adapter = albumAdapter
-    }
 
-    private fun retrieveSongs() {
-        val firestore = FirebaseFirestore.getInstance()
-        firestore.collection("Songs")
-            .get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val querySnapshot = task.result
-                    for (document in querySnapshot) {
-                        val songName = document.getString("songName")
-                        val songUrl = document.getString("songUrl")
-                        val songArtist = document.getString("songArtist")
-                        val songDuration = document.getString("songDuration")
-                        val imageUrl = document.getString("imageUrl")
-
-                        songsNameList.add(songName)
-                        songsUrlList.add(songUrl)
-                        songsArtistList.add(songArtist)
-                        songsDurationList.add(songDuration)
-                        thumbnail.add(imageUrl)
-                        jcAudios.add(JcAudio.createFromURL(songName.toString(), songUrl.toString()))
-                    }
-
-                    adapter = ListAdapter(
-                        applicationContext,
-                        songsNameList,
-                        thumbnail,
-                        songsArtistList,
-                        songsDurationList
-                    )
-
-                    jcPlayerView.initPlaylist(jcAudios, null)
-                    listView.adapter = adapter
-                    adapter.notifyDataSetChanged()
-                } else {
-                    Toast.makeText(this@OnlineMusic, "FAILED!", Toast.LENGTH_SHORT).show()
-                }
-            }
     }
 
     private fun retrieveAlbums() {
@@ -125,6 +79,16 @@ class OnlineMusic : AppCompatActivity() {
                         albumList.add(album)
                     }
                     albumAdapter.notifyDataSetChanged()
+
+                    // Hiển thị danh sách đầu gồm 3 album
+                    val firstThreeAlbums = albumList.take(3)
+                    albumAdapterTop = AlbumAdapter(this, firstThreeAlbums.toMutableList())
+                    topListView.adapter = albumAdapterTop
+
+                    // Hiển thị danh sách cuối cùng gồm 3 album
+                    val lastThreeAlbums = albumList.takeLast(3)
+                    albumAdapterBottom = AlbumAdapter(this, lastThreeAlbums.toMutableList())
+                    bottomListView.adapter = albumAdapterBottom
                 } else {
                     Toast.makeText(this, "Failed to retrieve albums", Toast.LENGTH_SHORT).show()
                 }
